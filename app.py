a71c0f402294207cea2caf1fca38e75d4fb61bf9
zhibin <617271321@qq.com>
2018-03-01 17:40:20 +0800

    day2
    day2

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-	return web.Response(body=b'<h1>Awesome</h1>')
+	return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
 
-@asyncio.coroutinue
+@asyncio.coroutine
 def init(loop):
 	app = web.Application(loop=loop)
-	app.router.add_router('GET', '/', indeex)
+	app.router.add_get('/', index)
 	srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
 	logging.info('server started at http://127.0.0.1:9000...')
 	return srv
 
-loop = asyncion.get_event_loop()
+loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
